d11e6d5603aa219c8a38c371cc7f2091471d0322
Jorge Sánchez <jsanchez9900@uta.edu.ec>
2025-06-04 21:46:11 -0500

    Primer commit - Subida inicial del proyecto Java

---
+++ src/pruebagithub/PruebaGitHub.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pruebagithub;
+
+/**
+ *
+ * @author Asus
+ */
+public class PruebaGitHub {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int num1 = 2;
+        int num2 = 6; 
+        int suma= 0;
+        suma = num1+num2;
+        
+        System.out.println("Los números ingresados son: " + num1 + " y " + num2);
+        System.out.println("La suma es: "+suma);
+    }
+    
+}
